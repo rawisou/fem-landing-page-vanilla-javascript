f916e89865b9bce151c651afef9ecd08d64451ba
Rawisara Saman <rawiisara@gmail.com>
2022-09-10 15:58:24 +0200

    nav and header added in mobile view

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 const menuToggle = document.querySelector("#menu-toggle");
 const activeElements = document.querySelectorAll(".active-element");
-const toggleMenu = menuToggle.addEventListener("click", function() {
-    activeElements.forEach(function(e) {
-        e.classList.toggle("active"); 
+
+// Option1 : Declare function inside addEventListener
+// const toggleMenu = menuToggle.addEventListener("click", function() {
+//     activeElements.forEach(function(e) {
+//         e.classList.toggle("active"); 
+//     }); 
+// });
+
+// Option 2 : use arrow function and invoke it in the event listener
+const addClass = () => {
+    activeElements.forEach(element => {
+        element.classList.toggle("active"); 
     }); 
-});
+}
+
+const toggleMenu = menuToggle.addEventListener("click", addClass);
